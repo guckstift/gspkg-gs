2628e5c8a2144016c335fd62ad12928ea259e53e
guckstift <guckstift@posteo.de>
2016-03-17 17:38:40 +0100

    added prim types

--- utils.h
+++ utils.h
@@ -8,9 +8,20 @@
 #define UNPACK3(A) (A)[0], (A)[1], (A)[2]
 #define UNPACK4(A) (A)[0], (A)[1], (A)[2], (A)[3]
 
+#define foreach(I,L) for (auto I = (L).begin (); I != (L).end (); ++I)
+
 namespace gs
 {
 	using namespace std;
+	
+	typedef int8_t i8;
+	typedef int16_t i16;
+	typedef int32_t i32;
+	typedef int64_t i64;
+	typedef uint8_t u8;
+	typedef uint16_t u16;
+	typedef uint32_t u32;
+	typedef uint64_t u64;
 	
 	struct Newline
 	{
